a35d42fbe7f560d8f1c6839d62cf49d4c619f50e
batoolalk <btlsharaf@gmail.com>
2025-10-04 04:46:30 +0300

    fixes css issue

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
